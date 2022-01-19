c2d36ba8549ad81920be0ef8029c05045688a31a
Philip <philipwinduong@gmail.com>
2022-01-18 21:53:21 -0800

    Add cron to emitter and update Mongo Schema

--- reminder_api/src/emitter/reminderEmitter.js
+++ reminder_api/src/emitter/reminderEmitter.js
@@ -1,4 +1,5 @@
 import EventEmitter from "events";
+import cron from "node-cron";
 import { changeReminderStatus } from "../controllers/emitterController.js";
 import { sendEmailReminder } from "../mailer/mailer.js"
 import { sendTextReminder } from "../texter/texter.js";
@@ -8,33 +9,56 @@ const eventEmitter = new EventEmitter();
 
 eventEmitter.on("test", () => {
     const currentTime = new Date().toLocaleTimeString();
-    console.log(`[${currentTime}]: Testing EventEmitter using GET Request`);
+    console.log(`[${currentTime}] Testing EventEmitter using GET Request`);
 });
 
 
 eventEmitter.on("RUN", reminder => {
+    console.log("emitted RUN event")
     let count = reminder.repeat; 
-    const miliseconds = (reminder.minutes * 60) * 1000;
+    // const miliseconds = (reminder.minutes * 60) * 1000;
 
-    const reminderInterval = setInterval(() => {
+    // const reminderInterval = setInterval(() => {
+    //     const currentTime = new Date().toLocaleTimeString();
+    //     console.log(`[${currentTime}] REMINDER: ${reminder.title} - ${reminder.content}. ${reminder.minutes}`)
+
+    //     if (reminder.enableSMS === true) {
+    //         eventEmitter.emit("TEXT", reminder);
+    //     };
+
+    //     if (reminder.enableEmail === true) {
+    //         eventEmitter.emit("EMAIL", reminder);
+    //     };
+
+    //     count = count - 1; 
+
+    //     if (count === 0) {
+    //         changeReminderStatus(reminder, "INACTIVE");
+    //         clearInterval(reminderInterval);
+    //     }
+    // }, miliseconds);
+    const cronSchedule = `${reminder.minutes} ${reminder.hour} ${reminder.day} ${reminder.month} ${reminder.weekday}`;
+    const cronTask = cron.schedule(cronSchedule, async () => {
         const currentTime = new Date().toLocaleTimeString();
-        console.log(`[${currentTime}] REMINDER: ${reminder.title} - ${reminder.content}. ${reminder.minutes}`)
+        
+        console.log(`[${currentTime}] REMINDER: ${reminder.title} - ${reminder.content}.`)
+        
+        if (reminder.enableEmail === true) {
+            eventEmitter.emit("EMAIL", reminder);
+        };
 
         if (reminder.enableSMS === true) {
             eventEmitter.emit("TEXT", reminder);
         };
 
-        if (reminder.enableEmail === true) {
-            eventEmitter.emit("EMAIL", reminder);
-        };
-
-        count = count - 1; 
-
+        count = count - 1;
+        console.log(`Count: ${count}`)
         if (count === 0) {
             changeReminderStatus(reminder, "INACTIVE");
-            clearInterval(reminderInterval);
-        }
-    }, miliseconds);
+            cronTask.stop();
+        };
+    });
+
 });
 
 
