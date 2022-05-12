b77b7561dc7339232bcbf5215e2cf67cf340f6c2
Philip <philipwinduong@gmail.com>
2022-05-11 22:48:16 -0700

    Add Babel and Webpack

---
+++ reminder_queue/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const nodeExternals = require('webpack-node-externals');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+const dotenv = require("dotenv");
+dotenv.config()
+
+module.exports = {
+	mode: process.env.MODE,
+	entry: "./src/index.js",
+	target: "node",
+	experiments: {
+		topLevelAwait: true
+	},
+	externals: [nodeExternals()],
+	output: {
+		filename: "server.bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [
+		new NodemonPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader',
+			  }
+		  ]
+	},
+}
