9af885fee15b3334f14faa5c829e043d886cce24
Philip <philipwinduong@gmail.com>
2022-05-12 16:49:28 -0700

    Add webpack to reminder_api

---
+++ reminder_api/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const NodemonPlugin = require("nodemon-webpack-plugin");
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports = (env) => {
+    const config = {
+        mode: env.mode,
+        entry: "./src/server.js",
+        target: "node",
+        experiments: {
+            topLevelAwait: true,
+        },
+        externals: [nodeExternals()],
+        output: {
+            filename: "server.bundle.js",
+            path: path.resolve(__dirname, "dist"),
+        },
+        plugins: [new NodemonPlugin()],
+        module: {},
+    };
+
+    return config;
+};
